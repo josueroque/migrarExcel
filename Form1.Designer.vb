29a4ac6d3a32b4191968b44066f3faf029260a12
Josue Roque <josueroque@yahoo.com>
2020-08-04 10:25:20 -0600

    migrando a excel correctamente

---
+++ Form1.Designer.vb
@@ -0,0 +1,138 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtCodPlanta = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Enabled = False
+        Me.Button1.Location = New System.Drawing.Point(354, 238)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(97, 31)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Leer y exportar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(107, 32)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(97, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Seleccionar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(259, 42)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(113, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Seleccione un archivo"
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(244, 197)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(333, 23)
+        Me.ProgressBar1.TabIndex = 4
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(302, 161)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(219, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Presione en Leer y Exportar para comenzar..."
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(354, 285)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(97, 27)
+        Me.Button3.TabIndex = 6
+        Me.Button3.Text = "Cerrar"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(113, 100)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(88, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Codigo de Planta"
+        '
+        'txtCodPlanta
+        '
+        Me.txtCodPlanta.Location = New System.Drawing.Point(262, 93)
+        Me.txtCodPlanta.Name = "txtCodPlanta"
+        Me.txtCodPlanta.Size = New System.Drawing.Size(100, 20)
+        Me.txtCodPlanta.TabIndex = 8
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.txtCodPlanta)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtCodPlanta As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,169 @@
+﻿Imports System
+Imports Microsoft.VisualBasic
+Imports System.Data
+Imports System.Data.Common
+Imports System.Data.SqlClient
+Imports System.Data.OleDb
+Imports Microsoft.WindowsAPICodePack.Dialogs
+Imports Microsoft.Office.Interop
+Imports Microsoft.Office.Interop.Excel
+Imports System.Globalization
+Imports System.Globalization.CultureInfo
+
+Public Class Form1
+    Public cn As New SqlConnection
+    Private counter As Double = 1
+    Private Function adoconecta()
+        Try
+            Dim strcn As String
+            strcn = "Server=LAPTOP-JOSUE\MSSQLSERVER14;database=Migracion;user id=migracionUser;password=ODS2020*;multipleactiveresultsets=true;"
+
+            cn = New SqlConnection(strcn)
+            cn.Open()
+
+            Return cn
+        Catch ex As Exception
+
+        End Try
+
+    End Function
+    Private Function adoconectaExcel()
+        '   Try
+        Dim cn2 As System.Data.OleDb.OleDbConnection
+        Dim strcn As String
+        strcn = "Provider=Microsoft.ACE.OLEDB.12.0;" & "Data Source=" & (Label1.Text) & ";" & "Extended Properties=" & Convert.ToChar(34).ToString() & "Excel 12.0" + Convert.ToChar(34).ToString() & ";"
+        cn2 = New System.Data.OleDb.OleDbConnection(strcn)
+        cn2.Open()
+        Return cn2
+        '  Catch ex As Exception
+
+        '    End Try
+
+    End Function
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim years(5) As String
+        years(0) = "2015"
+        years(1) = "2016"
+        years(2) = "2017"
+        years(3) = "2018"
+        years(4) = "2019"
+        Label2.Text = "Trabajando en ello..."
+        'Create a new excel file
+
+        Dim xlApp As New Excel.Application
+        Dim excelBook As Excel.Workbook = xlApp.Workbooks.Add
+        Dim excelFileName As String = "c:\archivos\Book-" & System.DateTime.Now.ToString.Replace(".", "").Replace(" ", "").Replace("/", "").Replace("-", "").Replace(":", "") & ".xlsx"
+
+        excelBook.SaveAs(excelFileName)
+        excelBook.Close()
+        Dim cuenta As Integer = 1
+
+        ProgressBar1.Maximum = years.Length
+        Dim xls As New Microsoft.Office.Interop.Excel.Application
+        Dim xlsWorkBook As Microsoft.Office.Interop.Excel.Workbook
+        xlsWorkBook = xls.Workbooks.Open(excelFileName)
+        For Each year As String In years
+            ProgressBar1.Value = cuenta
+            If Not IsNothing(year) Then
+                ProcessData(year, xls, xlsWorkBook)
+            End If
+
+            ' Application.DoEvents()
+
+            cuenta = cuenta + 1
+        Next
+        xlsWorkBook.Close()
+        xls.Quit()
+        Label2.Text = "Tarea finalizada"
+        Button1.Enabled = False
+        counter = 1
+        MessageBox.Show("Datos migrados con exito en el archivo " & excelFileName)
+
+    End Sub
+
+    Private Sub ProcessData(year, xls, xlsWorkBook)
+        Dim ssql As String
+        Dim conexion = adoconectaExcel()
+
+        ssql = "SELECT * FROM [GH_CRND_" & year & "$]"
+
+        Dim cmd As New OleDbCommand(ssql, conexion)
+        Dim lect As OleDbDataReader = cmd.ExecuteReader
+
+
+        'Modifies excel file
+
+
+        Dim xlsWorkSheet As Microsoft.Office.Interop.Excel.Worksheet
+        Dim misValue As Object = System.Reflection.Missing.Value
+
+
+        While lect.Read
+
+            xlsWorkSheet = xlsWorkBook.Sheets("hoja1")
+
+            If counter = 1 Then
+                xlsWorkSheet.Cells(counter, 1) = "CodigoPlanta"
+                xlsWorkSheet.Cells(counter, 2) = "FechaHora"
+                xlsWorkSheet.Cells(counter, 3) = "Potencia"
+                counter = counter + 1
+            Else
+                Dim columCounter = 1
+
+                While columCounter <= 24
+                    'Dim mes As String = lect(0).Month
+                    'If mes.Length = 1 Then
+                    '    '   mes = "0" & mes
+                    'End If
+                    'Dim dia As String = lect(0).Day
+                    'If dia.Length = 1 Then
+                    '    '    dia = "0" & dia
+                    'End If
+                    'Dim año As Integer = lect(0).Year
+                    Dim hora As Integer = columCounter - 1
+                    If columCounter = 1 Then
+                        hora = 0
+                    End If
+                    'If hora.ToString.Length = 1 Then
+                    ' hora = "0" & hora
+                    ' End If
+                    'Dim fecha As String = dia & "/" & mes & "/" & año & " " & hora & ":00:00"
+                    ' fecha.AddHours(columCounter - 1)
+                    Dim fecha1 = CDate(lect(0))
+                    Dim fecha As DateTime = New DateTime(fecha1.Year, fecha1.Month, fecha1.Day, hora, 0, 0, 0)
+                    xlsWorkSheet.Cells(counter, 1) = txtCodPlanta.Text
+                    xlsWorkSheet.Cells(counter, 2) = fecha
+                    xlsWorkSheet.Cells(counter, 3) = lect(columCounter)
+                    columCounter = columCounter + 1
+                    Label2.Text = counter & " filas escritas"
+                    counter = counter + 1
+                End While
+            End If
+
+
+        End While
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+
+        Dim openDialog As OpenFileDialog = New OpenFileDialog()
+        openDialog.Title = "Select A File"
+        openDialog.Filter = "All Files (*.*)|*.*"
+        If openDialog.ShowDialog() = DialogResult.OK Then
+
+            Dim file As String = openDialog.FileName
+            Label1.Text = file
+
+        End If
+        Button1.Enabled = True
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        End
+    End Sub
+End Class
